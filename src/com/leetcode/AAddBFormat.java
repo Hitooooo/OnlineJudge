d49f2743c2290e2b4fca42bc6ddf65c5dc8cb510
Mht <hitomeng@outlook.com>
2019-03-25 00:17:36 +0800

    Valid Parentheses

--- src/com/leetcode/AAddBFormat.java
+++ src/com/leetcode/AAddBFormat.java
@@ -1,3 +1,5 @@
+package com.leetcode;
+
 /**
  * Calculate a+b and output the sum in standard format -- that is,
  * the digits must be separated into groups of three by
---
+++ src/com/leetcode/ValidParentheses.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * @author HitoM
+ * @date 2019/3/25 0:01
+ */
+public class ValidParentheses {
+    public static boolean solution(String s) {
+        if (s == null) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (!stack.empty() && check(stack.peek(), chars[i])) {
+                stack.pop();
+            } else {
+                stack.push(chars[i]);
+            }
+        }
+        return stack.empty();
+    }
+
+    private static boolean check(char c1, char c2) {
+        boolean result = false;
+        switch (c1) {
+            case '(':
+                result = c2 == ')';
+                break;
+            case '[':
+                result = c2 == ']';
+                break;
+            case '{':
+                result = c2 == '}';
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+}
