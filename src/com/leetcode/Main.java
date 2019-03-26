23b81d7836b39021a946d25dfb05c6738805dcfa
Mht <hitomeng@outlook.com>
2019-03-26 17:46:16 +0800

    Merge Two List

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -7,6 +7,13 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ValidParentheses.solution("([)]"));
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(3);
+        System.out.println(MergeTwoList.solution(l1,l2));
     }
 }
