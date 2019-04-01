91ba50c520307ff52fcb1897f09f73874e1f21f5
Mht <hitomeng@outlook.com>
2019-04-01 10:24:34 +0800

    Symmetric Tree and move common struct in one package

--- src/com/leetcode/MergeTwoList.java
+++ src/com/leetcode/MergeTwoList.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
 
+import com.leetcode.struct.ListNode;
+
 /**
  * Merge two sorted linked lists and return it as a new list.
  * The new list should be made by splicing together the nodes of the first two lists.
