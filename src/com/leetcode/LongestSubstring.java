fdb9b8ffac18afba093ed34228e617b4630a426a
Mht <hitomeng@outlook.com>
2019-04-17 23:15:44 +0800

    slide window

--- src/com/leetcode/LongestSubstring.java
+++ src/com/leetcode/LongestSubstring.java
@@ -5,6 +5,14 @@ import java.util.HashMap;
 /**
  * Given a string, find the length of the longest substring without repeating characters.
  *
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
  * @author HitoM
  * @date 2019/3/16 21:13
  */
@@ -17,6 +25,7 @@ public class LongestSubstring {
         int max = 0;
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0, j = 0; i < s.length(); i++) {
+            // j代表出现重复时，新的可用起始位置。
             if (map.keySet().contains(s.charAt(i))) {
                 j = Math.max(j, map.get(s.charAt(i)) + 1);
             }
