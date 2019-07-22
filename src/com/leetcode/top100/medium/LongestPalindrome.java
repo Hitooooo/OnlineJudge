391ab4344a1fbb2a181145b1c1c850100037db21
Mht <hitomeng@outlook.com>
2019-07-23 00:17:22 +0800

    i am tired

--- src/com/leetcode/top100/medium/LongestPalindrome.java
+++ src/com/leetcode/top100/medium/LongestPalindrome.java
@@ -1,5 +1,12 @@
 package com.leetcode.top100.medium;
 
+/**
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume that the maximum length of s is 1000.
+ * https://segmentfault.com/a/1190000003914228
+ *
+ * 从中心扩张法 分奇数 偶数个
+ */
 public class LongestPalindrome{
 
 }
