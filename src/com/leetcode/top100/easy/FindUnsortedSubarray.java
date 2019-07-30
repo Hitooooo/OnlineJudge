3335713882285809e60cc555840ea07b62033bf7
Mht <hitomeng@outlook.com>
2019-07-30 22:47:18 +0800

    longest palindrome substring by dynamic program

--- src/com/leetcode/top100/easy/FindUnsortedSubarray.java
+++ src/com/leetcode/top100/easy/FindUnsortedSubarray.java
@@ -1,6 +1,5 @@
 package com.leetcode.top100.easy;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
--- src/com/leetcode/top100/medium/LongestPalindrome.java
+++ src/com/leetcode/top100/medium/LongestPalindrome.java
@@ -49,4 +49,46 @@ public class LongestPalindrome {
         }
         return result;
     }
+
+    /**
+     * 动态规划的方式来解 对于起始地址i，结束地址为j的子串，用二维数组dp[i,j]表示，那么 如果 dp[i, j] = dp[i+1, j-1] &
+     * s[i]=s[j]; j - i > 1 true ; i == j s[i] == s[j] ; j - i =1
+     * 
+     * @return
+     */
+    public String longestDynamic(String s) {
+        // 边界值
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+
+        // 声明备忘录
+        boolean[][] dp = new boolean[s.length()][s.length()];
+
+        String result = s.substring(0, 1);
+        // 为什么这样不行，i是以来与i+1的，这种写法没有先计算i+1
+        /* for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1]);
+                if (dp[i][j]) {
+                    if (j - i + 1 > result.length()) {
+                        result = s.substring(i, j + 1);
+                    }
+                }
+            }
+        } */
+        for (int j = 0; j < s.length(); j++){
+            // 这样，就会先计算 dp[i+1][j-1]
+            for (int i = 0; i <= j; i++){
+                dp[i][j] = s.charAt(i) == s.charAt(j) &&(j - i <= 2 || dp[i+1][j-1]);
+                // 与暂时最长进行比较
+                if (dp[i][j]){
+                    if (j - i + 1 > result.length()){
+                        result = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
