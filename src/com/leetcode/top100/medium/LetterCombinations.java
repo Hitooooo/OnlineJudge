f92b0cb068dd2b99f81db745bddb809631bad24f
Mht <hitomeng@outlook.com>
2019-08-14 23:24:44 +0800

    letter Combinations 暴力

--- src/com/leetcode/top100/medium/LetterCombinations.java
+++ src/com/leetcode/top100/medium/LetterCombinations.java
@@ -1,5 +1,9 @@
 package com.leetcode.top100.medium;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +18,7 @@ public class LetterCombinations {
 
     /**
      * 链表保存前一个数字的可能的数字关系，下一个数字获取后拼接自己可能的字母。
+     *
      * @param digits
      * @return
      */
@@ -23,7 +28,7 @@ public class LetterCombinations {
             return result;
         }
         result.add("");
-        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         for (int i = 0; i < digits.length(); i++) {
             int index = Character.getNumericValue(digits.charAt(i));
             while (result.peek().length() == i) {
@@ -35,5 +40,40 @@ public class LetterCombinations {
             }
         }
         return result;
+    }
+
+    public List<String> letterCombinations2(String digits) {
+        ArrayList<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (int i = 0; i < digits.length(); i++) {
+            int index = Character.getNumericValue(digits.charAt(i));
+            String s = mapping[index];
+            if (result.size() == 0) {
+                for (int k = 0; k < s.length(); k++) {
+                    result.add(s.charAt(k) + "");
+                }
+            } else {
+                ArrayList<String> temp = new ArrayList<>();
+                for (int j = 0; j < s.length(); j++) {
+                    for (String s1 : result) {
+                        temp.add(s1 + s.charAt(j));
+                    }
+                }
+                result.clear();
+                result.addAll(temp);
+            }
+
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        assert letterCombinations2("2345").equals(letterCombinations("2345"));
+//        List<String> strings = letterCombinations2("2345");
+//        System.out.println(strings);
     }
 }
