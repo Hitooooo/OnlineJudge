d02ca1d10f9e0986bd4c98e73a284c635f93d693
Mht <hitomeng@outlook.com>
2019-09-07 23:55:42 +0800

    Emmmmm

---
+++ src/com/leetcode/top100/medium/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package com.leetcode.top100.medium;
+
+import java.util.List;
+
+/**
+ * Given an array of strings, group anagrams together.
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 Input : ["eat", "tea", "tan",
+ * "ate", "nat", "bat"], Output : [ ["ate","eat","tea"], ["nat","tan"], ["bat"]
+ * ]
+ * 
+ * @author hitomeng
+ * @date 2019/09/07
+ */
+public class GroupAnagrams {
+
+    /**
+     * 写一个算法，将具有相同字符（可不同顺序）的字符串映射成相同的字符串，然后通过Map的put操作
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return null;
+    }
+}
