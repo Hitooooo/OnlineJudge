66307aad7378ea11482b6bfc1f79a68106625c9c
Mht <hitomeng@outlook.com>
2019-09-17 21:46:08 +0800

    binary tree traversal post and pre

--- src/com/leetcode/top100/medium/BinaryTreeInorderTravelsal.java
+++ src/com/leetcode/top100/medium/BinaryTreeInorderTravelsal.java
@@ -1,6 +1,7 @@
 package com.leetcode.top100.medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import com.leetcode.struct.TreeNode;
@@ -15,6 +16,47 @@ import org.junit.Test;
  */
 public class BinaryTreeInorderTravelsal {
 
+    public List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> temp = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                temp.add(root.val);
+                stack.push(root);
+                root = root.right;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                root = root.left;
+            }
+        }
+        Collections.reverse(temp);
+        return temp;
+    }
+
+    /**
+     * 非递归的先序遍历呢 
+     * a. 遇到一个节点，访问它，然后把它压栈，并去遍历它的左子树； 
+     * b.当左子树遍历结束后，从栈顶弹出该节点并将其指向右儿子，继续a步骤；
+     * c. 当所有节点访问完即最后访问的树节点为空且栈空时，停止。
+     */
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> temp = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                temp.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                root = root.right;
+            }
+        }
+        return temp;
+    }
+
     /**
      * 一般改为非递归，使用栈来保存需要用到的信息
      */
@@ -22,7 +64,7 @@ public class BinaryTreeInorderTravelsal {
         List<Integer> temp = new ArrayList<>();
         LinkedList<TreeNode> stack = new LinkedList<>();
         while (root != null || !stack.isEmpty()) {
-            while(root != null){
+            while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
@@ -59,7 +101,7 @@ public class BinaryTreeInorderTravelsal {
         TreeNode node2 = new TreeNode(2);
         root.right = node2;
         node2.left = new TreeNode(3);
-        List<Integer> result = inorderTraversal(root);
+        List<Integer> result = preOrderTraversal(root);
         System.out.println(result);
     }
 }
