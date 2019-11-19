45e59e3d05e65c2fe282beb0b0c4a93cbf1639b2
Hitooooo <hitomeng@outlook.com>
2019-11-19 14:29:59 +0800

    evaluate division 339

---
+++ src/com/leetcode/top100/medium/EvaluateDivision399.java
@@ -0,0 +1,21 @@
+package com.leetcode.top100.medium;
+
+import java.util.List;
+
+/**
+ * @author hitomeng
+ * @date 2019/11/19
+ */
+public class EvaluateDivision399{
+    /**
+     * 构造一个双向图。
+     * 
+     * @param equations 等式
+     * @param values    等式结果
+     * @param queries   需要计算对
+     * @return  计算结果
+     */
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+}
