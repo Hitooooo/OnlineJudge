998fa5b8d8e64c7e5fd928aa9c280d204ac63275
Mht <hitomeng@outlook.com>
2020-03-12 23:59:55 +0800

    sort quick

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -8,15 +8,36 @@ import com.leetcode.top100.medium.LRUByDoubleList;
 public class Main {
 
     public static void main(String[] args) {
-        LRUByDoubleList list = new LRUByDoubleList(2);
-        list.put(1, 1);
-        list.put(2, 2);
-        int n1 = list.get(1);
-        list.put(3, 3);
-        int n2 = list.get(2);
-        list.put(4, 4);
-        int n3 = list.get(1);
-        int n4 = list.get(3);
-        int n5 = list.get(4);
+        int[] arr = {2,3,4,5,4,1};
+        System.out.println(partition(arr, 0, 5));
     }
+
+
+    public static int partition(int arr[], int begin, int end) {
+        int pov = begin;
+        int povValue = arr[pov];
+
+        int l = begin;
+        int r = end;
+        while(l < r){
+            while(arr[r] >= povValue && l < r){
+                r--;
+            }
+            while(arr[l] <= povValue && l < r){
+                l++;
+            }
+            swap(arr, l, r);
+        }
+        // 将被看作pov的首个元素放到该返回的位置，也就是跳出循环时l=r的位置，必须保证l=r是小于等于povValue的
+        // 所以whlie循环里面的第一个必须是从右侧开始，保证r指向的一定是小于povValue的。
+        swap(arr, begin, r);
+        return r;
+    }
+
+    public static void swap(int[] arr ,int l ,int r){
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+
 }
