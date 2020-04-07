bf31523b936ea7f37d5c2e4269e27690fce23565
Mht <hitomeng@outlook.com>
2020-04-07 23:51:01 +0800

    区域和检索 - 数组不可变

---
+++ src/com/leetcode/dp/NumArray303.java
@@ -0,0 +1,20 @@
+package com.leetcode.dp;
+
+/**
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * @author hitomeng
+ * @date 2020-04-07 23:49:31
+ */
+public class NumArray303 {
+
+    private int[] prefixSum;
+
+    public NumArray303(int[] nums) {
+
+    }
+    
+    public int sumRange(int i, int j) {
+        return 0;
+    }
+}
