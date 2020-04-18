fa8e5f71a87f9c95c6fcc34e99fa4f0d744d257d
Mht <hitomeng@outlook.com>
2020-04-18 16:37:07 +0800

    excel col mapping

--- src/com/leetcode/readily/ExcelCol168.java
+++ src/com/leetcode/readily/ExcelCol168.java
@@ -2,10 +2,44 @@ package com.leetcode.readily;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.Test;
 
+/**
+ * ???????????? Excel ??????????
+ * 
+ * @author hitomeng
+ * @date 2020-04-15 23:29:35
+ */
 public class ExcelCol168 {
+
+    /**
+     * ?????????? 0 ????????? 26 ?????? 26 ? 1?????? 0?????? 26 ???? 26 ???? 27
+     * ???????? 1?????? 1??? Z(26) ???????? A(1)A(1)?? 27 ?? AA?
+     * 
+     * @param n
+     * @return
+     */
+    public String convertToTitle(int n) {
+        String res = "";
+        while (n > 0) {
+            int c = n % 26;
+            if (c == 0) {
+                // ??????1?
+                c = 26;
+                n -= 1;
+            }
+            res = (char) ('A' + c - 1) + res;
+            n = n / 26;
+        }
+        return res;
+    }
+
+    /**
+     * ?????????????
+     * 
+     * @param col
+     * @return
+     */
     public String mapping(int col) {
         char A = 'A';
         Map<Integer, Character> map = new HashMap<>();
@@ -19,13 +53,12 @@ public class ExcelCol168 {
             res = map.get(k) + res;
             col = col / 26;
         }
-        return  res;
+        return res;
     }
-    // TODO 借1法
-    
+
     @Test
     public void test() {
-        String res = mapping(52);
+        String res = convertToTitle(52);
         System.out.println(res);
     }
 }
