20c773592c55161b0fec4775e14d7f8f603cc9bc
Hitooooo <hitomeng@outlook.com>
2020-09-04 15:39:53 +0800

    lru cache

---
+++ src/com/leetcode/datastruct/LRUCache.java
@@ -0,0 +1,18 @@
+package com.leetcode.datastruct;
+
+public class LRUCache<K,V>{
+    private int capacity;
+
+    public boolean put(K k, V v){
+        return false;
+    }
+
+    public boolean delete(K k){
+        return false;
+    }
+
+    public V get(K k){
+        return null;
+    }
+
+}
